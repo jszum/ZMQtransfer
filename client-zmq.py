325719c35e341301feeb65b7a0f078d777d562a8
jszum <jaroslaw.szumega@gmail.com>
2015-07-28 18:16:46 +0200

    Broadcast method added, names refactoring

--- client-zmq.py
+++ client-zmq.py
@@ -8,23 +8,31 @@ class ZMQClient:
     def __init__(self):
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REQ)
-        self.address = ''
-        self.port = ''
+        self.server_ip = []
+        self.server_port = []
 
     def connect(self, adr, port):
-        self.address = adr
-        self.port = port
+        self.server_ip.append(adr)
+        self.server_port.append(port)
 
     def send(self):
-        self.socket.connect('tcp://%s:%s' % (self.address, self.port))
+        self.socket.connect('tcp://%s:%s' % (self.server_ip[-1], self.server_port[-1]))
         self.socket.send('Test message')
-        print 'sended to ' + 'tcp://%s:%s' % (self.address, self.port)
+        print 'Sent to ' + 'tcp://%s:%s' % (self.server_ip[-1], self.server_port[-1])
+
+    def broadcast(self):
+        for el in range(len(self.server_ip)):
+            self.socket.connect("tcp://%s:%s" % (self.server_ip[el], self.server_port[el]))
+            self.socket.send('Broadcasting message')
+            print 'Sent to ' + 'tcp://%s:%s' % (self.server_ip[el], self.server_port[el])
+
+
 
 if __name__ == "__main__":
-    qaddress = sys.argv[1]
-    qport = sys.argv[2]
-    print "Connect to %s:%s" % (qaddress, qport)
+    address = sys.argv[1]
+    port = sys.argv[2]
+    print "Connect to %s:%s" % (address, port)
 
     client = ZMQClient()
-    client.connect(qaddress, qport)
+    client.connect(address, port)
     client.send()
