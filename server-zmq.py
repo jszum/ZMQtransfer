1c345398013d068ddcadacd7f8217a058112c7c0
jszum <jaroslaw.szumega@gmail.com>
2015-07-28 12:08:06 +0200

    ZMQ server for message receiving

--- server-zmq.py
+++ server-zmq.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
 
 import zmq
+import sys
+
+class ZMQserver:
+
+    def __init__(self, port=5556):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REP)
+        self.socket.bind("tcp://*:%s" % port)
+        self.port = port
+
+    def run(self):
+        print 'Server is running'
+        while True:
+            message = self.socket.recv()
+            print message
 
 
 
+if __name__ == "__main__":
+    qport = int(sys.argv[1])
+    print qport
+
+    server = ZMQserver()
+    server.run()
