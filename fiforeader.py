6806c5a5f8f2cdf8dbd17e6cca274728b953132a
jszum <jaroslaw.szumega@gmail.com>
2015-08-06 16:46:39 +0200

    Script that reads data from FIFO and prints it

---
+++ fiforeader.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from struct import *
+import os
+fifo = 'zmqfifo'
+
+class Reader:
+
+    def __init__(self, port=54001):
+        self.readings = 192*4
+        self.fifo = fifo
+
+    def run(self):
+        bin = os.open(self.fifo, os.O_RDWR)
+
+        while True:
+            record = ''
+            msg = os.read(bin, 24*self.readings)
+            for i in range(self.readings):
+                    buffer = msg[24*i:24*(i+1)]
+                    if len(buffer)==24:
+                        unpck = unpack('<Qhhhhhhhh', buffer)
+                        record += ';'.join(str(e) for e in unpck)+';\n'
+            print record
+
+
+def clean():
+    try:
+        os.unlink(fifo)
+    except:
+        pass
+    os.mkfifo(fifo)
+
+if __name__ == "__main__":
+
+    clean()
+    reader = Reader()
+    reader.run()
