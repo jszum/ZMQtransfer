40ddc89b5cc66ab712d20eb24251d0e8e7a48f54
jszum <jaroslaw.szumega@gmail.com>
2015-08-06 18:26:01 +0200

    Spliting data into 8 channels for visualiser

--- fiforeader.py
+++ fiforeader.py
@@ -2,6 +2,8 @@
 
 from struct import *
 import os
+import array
+
 fifo = 'zmqfifo'
 
 class Reader:
@@ -15,25 +17,49 @@ class Reader:
 
         while True:
             record = ''
+            rawdata = []
+
+            for i in range(8):
+                rawdata.append(array.array('h'))
+
             msg = os.read(bin, 24*self.readings)
             for i in range(self.readings):
-                    buffer = msg[24*i:24*(i+1)]
-                    if len(buffer)==24:
-                        unpck = unpack('<Qhhhhhhhh', buffer)
-                        record += ';'.join(str(e) for e in unpck)+';\n'
+                buffer = msg[24*i:24*(i+1)]
+
+                if len(buffer) == 24:
+                    unpck = unpack('<Qhhhhhhhh', buffer)
+                    for f in range(8):
+                        rawdata[f].append(unpck[f+1])
+
+                    record += ';'.join(str(e) for e in unpck)+';\n'
+
             self.saveCSV(record)
+            self.split(rawdata)
 
     def saveCSV(self, record):
         csv = open('data.csv', 'wa+')
         csv.write(record)
         csv.close()
 
+    def split(self, rec):
+        for f in range(8):
+            data = pack('h'*self.readings, *rec[f])
+            file = open('fifo'+str(f), 'wab+')
+            file.write(data)
+            file.close()
+
+
+
 def clean():
     try:
         os.unlink(fifo)
+        for i in range(8):
+            os.unlink('fifo'+str(i))
     except:
         pass
     os.mkfifo(fifo)
+    for i in range(8):
+        os.mkfifo('fifo'+str(i))
 
 if __name__ == "__main__":
 
